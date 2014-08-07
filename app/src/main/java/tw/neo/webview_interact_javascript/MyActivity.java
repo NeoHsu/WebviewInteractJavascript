650d536ae35605bfa15081dc31dc15970bd925be
Neo Hsu <NeoHsuDroid@gmail.com>
2014-08-07 15:03:45 +0800

    Initial commit

---
+++ app/src/main/java/tw/neo/webview_interact_javascript/MyActivity.java
@@ -0,0 +1,58 @@
+package tw.neo.webview_interact_javascript;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import org.json.JSONArray;
+import java.util.Arrays;
+
+public class MyActivity extends ActionBarActivity {
+    WebView webview;
+    String mStringArray[] = { "String1", "String2" };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        webview = (WebView) this.findViewById(R.id.webView1);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.addJavascriptInterface(this, "android");
+        webview.requestFocusFromTouch();
+        webview.setWebViewClient(new WebViewClient());
+        webview.setWebChromeClient(new WebChromeClient());
+        webview.loadUrl("file:///android_asset/web.html");
+    }
+
+    @JavascriptInterface
+    public String getData() {
+        JSONArray mJSONArray = new JSONArray(Arrays.asList(mStringArray));
+        return mJSONArray.toString();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
